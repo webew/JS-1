bfc47498457d1d44df6662aa4cfbb442d2935688
FabriqueNumerique <gabbadie@step-sa.fr>
2018-05-22 13:55:13 +0200

    Détails sur l'exercice 4

--- exos_JS/EX4/JS/img.js
+++ exos_JS/EX4/JS/img.js
@@ -1,8 +1,14 @@
+
 var article = document.querySelector("article");
+// Attribution des styles d'arrière plan des divs
+article.children[0].style.background = "url(./images/paysage_1.jpg) 50% 50% no-repeat";
+article.children[1].style.background = "url(./images/paysage_2.jpg) 50% 50% no-repeat";
 // clic sur l'article contenant les div
 article.onclick = function(){
-    // Boucle dans les enfants de l'article. for .. of permet de récupérer les enfants directement
-    for(let el of this.children){
-        el.classList.toggle("invisible");
+    // Boucle dans les enfants de l'article. for .. of permet de récupérer les enfants 
+    // This pointe sur l'élément qui a été cliqué
+    console.log(this.children, this);
+    for(let pascal of this.children){
+        pascal.classList.toggle("invisible");
     }
 }
