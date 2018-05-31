b43bacc1967a7628e22252a8eda22a53764a2262
bf <fillion.bertrand@gmail.com>
2018-05-31 13:51:10 +0200

    ex14

--- Algorithmes/exercices_entrainement/ex14/js/ex14.js
+++ Algorithmes/exercices_entrainement/ex14/js/ex14.js
@@ -1,15 +1,10 @@
 //variables
 var tableau = [15, 3, 56, 1, 25];
 
-// console.log(tableau[6]);
-
 var somme = 0;
 
 var valeurMax = tableau[0];
 var valeurMin = tableau[0];
-
-//le tableau
-// console.log("Tableau : "+tableau);
 
 // somme
 for (let i = 0; i < tableau.length; i++) {
@@ -17,7 +12,6 @@ for (let i = 0; i < tableau.length; i++) {
 }
 
 console.log("Somme : "+somme);
-
 
 //plus grande valeur (max)
 for (let i = 1; i < tableau.length; i++) {
