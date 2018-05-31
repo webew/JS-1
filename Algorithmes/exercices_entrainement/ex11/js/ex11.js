069590141d95d35280f884da6af885af2d9045f5
bf <fillion.bertrand@gmail.com>
2018-05-31 08:39:52 +0200

    ex11

---
+++ Algorithmes/exercices_entrainement/ex11/js/ex11.js
@@ -0,0 +1,18 @@
+var veteran = 40;
+var senior = 23;
+var espoir = 20;
+var junior = 18;
+
+var age = 60;
+
+if (age >= veteran) {
+    alert("Veteran");
+} else if (age >= senior) {
+    alert("Senior");
+} else if (age >= espoir) {
+    alert("Espoir");
+} else if (age >= junior) {
+    alert("Junior");
+} else {
+    alert("?????");
+}
