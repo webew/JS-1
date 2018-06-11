122b3a43801b603778cc9f3e3cc5b22f07a72a20
bf <fillion.bertrand@gmail.com>
2018-06-11 10:20:07 +0200

    ajout fonctions

---
+++ fonctions/README.md
@@ -0,0 +1,16 @@
+# Répète Jacquot !
+
+<p>Votre collègue développeur maîtrise visiblement le copier-coller.</p>
+<p>Lorsqu'il aura créé 20 chapitres, son code approchera les 200 lignes !</a>
+<p>De plus, s'il souhaite modifier la structure des chapitres, il devra tous les modifier, un par un !</p>
+<p>Aidez-le à automatiser son code !</p>
+
+## DRY !
+### Avec une fonction
+<p>On peut ici créer une fonction qui générera un chapitre.</p>
+
+### Mettre les données dans un fichier json
+<p>Il serait plus facile de gérer les données (titre, texte) dans un fichier json, chargé en Ajax...</p>
+
+### La classe !
+<p>ça ne vous blûle pas les doigts de créer une classe ?</p>
---
+++ fonctions/js/chapitres.js
@@ -0,0 +1,35 @@
+var maDiv = document.createElement("div");
+var monTitre = document.createElement("h1");
+var monParagraphe = document.createElement("p");
+monTitre.innerText = "Bienvenue !";
+monParagraphe.innerText = "Bienvenue sur mon site ! Plein de lorem ipsum...";
+maDiv.appendChild(monTitre);
+maDiv.appendChild(monParagraphe);
+document.body.appendChild(maDiv);
+
+var maDiv1 = document.createElement("div");
+var monTitre1 = document.createElement("h1");
+var monParagraphe1 = document.createElement("p");
+monTitre1.innerText = "Chapitre 1 : un chapitre pour débuter";
+monParagraphe1.innerText = "Paragraphe chapitre 1";
+maDiv1.appendChild(monTitre1);
+maDiv1.appendChild(monParagraphe1);
+document.body.appendChild(maDiv1);
+
+var maDiv3 = document.createElement("div");
+var monTitre3 = document.createElement("h1");
+var monParagraphe3 = document.createElement("p");
+monTitre3.innerText = "Chapitre 2 : un autre chapitre";
+monParagraphe3.innerText = "Paragraphe chapitre 2";
+maDiv3.appendChild(monTitre3);
+maDiv3.appendChild(monParagraphe3);
+document.body.appendChild(maDiv3);
+
+var maDiv4 = document.createElement("div");
+var monTitre4 = document.createElement("h1");
+var monParagraphe4 = document.createElement("p");
+monTitre4.innerText = "Chapitre 3 : encore un chapitre";
+monParagraphe4.innerText = "Paragraphe chapitre 3";
+maDiv4.appendChild(monTitre4);
+maDiv4.appendChild(monParagraphe4);
+document.body.appendChild(maDiv4);
